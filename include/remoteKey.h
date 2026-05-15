87707c40fa69dcae37a698e99ce53118f96d9c6a
yuldakim <jeong8003@gmail.com>
2026-05-15 16:20:07 +0900

    Test4. Red/ TVController.h 다른버튼에 대한 처리가 없음

--- include/remoteKey.h
+++ include/remoteKey.h
@@ -13,7 +13,20 @@
 
 #include <string>
 
-enum class remoteKey { KEY_1, KEY_2, KEY_3, KEY_4, KEY_OK };
+enum class remoteKey {
+  KEY_0,
+  KEY_1,
+  KEY_2,
+  KEY_3,
+  KEY_4,
+  KEY_5,
+  KEY_6,
+  KEY_7,
+  KEY_8,
+  KEY_9,
+  KEY_OK,
+  KEY_MENU
+};
 
 inline std::string to_string(remoteKey key) {
   switch (key) {
--- test/TVControllerTest.cpp
+++ test/TVControllerTest.cpp
@@ -48,3 +48,23 @@ TEST(TVControllerTest, S1_3_ContinuousInput) {
   controller.pushButton(remoteKey::KEY_3);
   controller.pushButton(remoteKey::KEY_4);
 }
+
+TEST(TVControllerTest, S1_4_OtherButtonShouldInvalidateInput) {
+  MockTunerForController mockTuner;
+
+  EXPECT_CALL(mockTuner, setCH("45")).Times(1);
+  EXPECT_CALL(mockTuner, setCH("6")).Times(0); // 호출되면 안 됨
+
+  TVController controller(&mockTuner);
+
+  // 4, 5 입력 -> "45" 자동 변경 (S1-2 로직에 의해 통과 예상)
+  controller.pushButton(remoteKey::KEY_4);
+  controller.pushButton(remoteKey::KEY_5);
+
+  // 6 입력 후 메뉴 버튼 클릭 -> 6 무효화
+  controller.pushButton(remoteKey::KEY_6);
+  controller.pushButton(remoteKey::KEY_MENU); // 이 순간 버퍼가 비워져야 함
+
+  // 확인을 위해 OK를 눌러도 아무 일 없어야 함 (이미 비워졌으니까)
+  controller.pushButton(remoteKey::KEY_OK);
+}
