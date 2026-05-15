fb289ac3404a7346b0ea00aab4e919ddda517775
yuldakim <jeong8003@gmail.com>
2026-05-15 17:31:32 +0900

    s2-2. Red / 같은채널 추가 시 삭제기능

--- build/FavoriteChannelTest[1]_tests.cmake
+++ build/FavoriteChannelTest[1]_tests.cmake
@@ -1,3 +1,5 @@
 add_test([=[FavoriteChannelTest.S2_1_AddCurrentChannelToFavorites]=]  C:/DEV_BR/TDD_TV_06/build/FavoriteChannelTest.exe [==[--gtest_filter=FavoriteChannelTest.S2_1_AddCurrentChannelToFavorites]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[FavoriteChannelTest.S2_1_AddCurrentChannelToFavorites]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV_BR\TDD_TV_06\test\FavoriteChannelTest.cpp:28]==] WORKING_DIRECTORY C:/DEV_BR/TDD_TV_06/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  FavoriteChannelTest_TESTS FavoriteChannelTest.S2_1_AddCurrentChannelToFavorites)
+set_tests_properties([=[FavoriteChannelTest.S2_1_AddCurrentChannelToFavorites]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV_BR\TDD_TV_06\test\FavoriteChannelTest.cpp:27]==] WORKING_DIRECTORY C:/DEV_BR/TDD_TV_06/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+add_test([=[FavoriteChannelTest.S2_2_ToggleFavoriteRemoveIfExisted]=]  C:/DEV_BR/TDD_TV_06/build/FavoriteChannelTest.exe [==[--gtest_filter=FavoriteChannelTest.S2_2_ToggleFavoriteRemoveIfExisted]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[FavoriteChannelTest.S2_2_ToggleFavoriteRemoveIfExisted]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV_BR\TDD_TV_06\test\FavoriteChannelTest.cpp:40]==] WORKING_DIRECTORY C:/DEV_BR/TDD_TV_06/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  FavoriteChannelTest_TESTS FavoriteChannelTest.S2_1_AddCurrentChannelToFavorites FavoriteChannelTest.S2_2_ToggleFavoriteRemoveIfExisted)
--- test/FavoriteChannelTest.cpp
+++ test/FavoriteChannelTest.cpp
@@ -3,7 +3,6 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-
 using namespace ::testing;
 
 // 추가: 여기서 MockTuner를 정의해줘야 아래 TestFixture에서 쓸 수 있습니다.
@@ -36,4 +35,19 @@ TEST_F(FavoriteChannelTest, S2_1_AddCurrentChannelToFavorites) {
   std::vector<int> favs = controller->getFavorites();
   ASSERT_EQ(1, favs.size()); // 목록에 1개가 있어야 함
   EXPECT_EQ(10, favs[0]);    // 그 값이 10이어야 함
+}
+
+TEST_F(FavoriteChannelTest, S2_2_ToggleFavoriteRemoveIfExisted) {
+  // 1. Given: 현재 채널이 "7"번이라고 가정
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("7"));
+
+  // 2. When: 첫 번째 클릭 (추가)
+  controller->pushButton(remoteKey::KEY_FAV);
+  ASSERT_EQ(1, controller->getFavorites().size()); // 일단 1개 들어간 것 확인
+
+  // 3. When: 두 번째 클릭 (삭제/토글)
+  controller->pushButton(remoteKey::KEY_FAV);
+
+  // 4. Then: 목록이 다시 비어있어야 함!
+  EXPECT_EQ(0, controller->getFavorites().size());
 }
