c6dc5f7cc2b38ac3673d59674323eeff33ca2f5f
yuldakim <jeong8003@gmail.com>
2026-05-15 15:37:35 +0900

    1번 테스트 코드 (RED)

--- test/TVControllerTest.cpp
+++ test/TVControllerTest.cpp
@@ -1,15 +1,25 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include "Tuner.h"
 #include "TVController.h"
+#include "Tuner.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
 
 class MockTunerForController : public Tuner {
 public:
-    MOCK_METHOD(std::string, seekCH, (), (override));
-    MOCK_METHOD(void, setCH, (const std::string& ch), (override));
-    MOCK_METHOD(std::string, getCurrentCH, (), (override));
+  MOCK_METHOD(std::string, seekCH, (), (override));
+  MOCK_METHOD(void, setCH, (const std::string &ch), (override));
+  MOCK_METHOD(std::string, getCurrentCH, (), (override));
 };
 
-TEST(TVControllerTest, testFramework) {
-    // fail();
+TEST(TVControllerTest, Press1AndConfirmShouldChangeToChannel1) {
+  MockTunerForController mockTuner;
+
+  // 1. 기대 정의: setCH("1")이 반드시 호출되어야 한다!
+  EXPECT_CALL(mockTuner, setCH("1")).Times(1);
+
+  TVController controller(&mockTuner);
+
+  // 2. 실행: 1번 누르고 OK 누르기
+  controller.pushButton(remoteKey::KEY_1);
+  controller.pushButton(remoteKey::KEY_OK);
 }
