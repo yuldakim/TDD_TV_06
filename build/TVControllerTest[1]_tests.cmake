75e25c4e28884dfe624963a3a041ee01e1f5f411
yuldakim <jeong8003@gmail.com>
2026-05-15 15:47:31 +0900

    Test2. Red

--- build/TVControllerTest[1]_tests.cmake
+++ build/TVControllerTest[1]_tests.cmake
@@ -1,3 +1,5 @@
 add_test([=[TVControllerTest.Press1AndConfirmShouldChangeToChannel1]=]  C:/DEV_BR/TDD_TV_06/build/TVControllerTest.exe [==[--gtest_filter=TVControllerTest.Press1AndConfirmShouldChangeToChannel1]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[TVControllerTest.Press1AndConfirmShouldChangeToChannel1]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV_BR\TDD_TV_06\test\TVControllerTest.cpp:14]==] WORKING_DIRECTORY C:/DEV_BR/TDD_TV_06/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  TVControllerTest_TESTS TVControllerTest.Press1AndConfirmShouldChangeToChannel1)
+set_tests_properties([=[TVControllerTest.Press1AndConfirmShouldChangeToChannel1]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV_BR\TDD_TV_06\test\TVControllerTest.cpp:13]==] WORKING_DIRECTORY C:/DEV_BR/TDD_TV_06/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+add_test([=[TVControllerTest.PressMultipleDigitsAndConfirmShouldChangeChannel]=]  C:/DEV_BR/TDD_TV_06/build/TVControllerTest.exe [==[--gtest_filter=TVControllerTest.PressMultipleDigitsAndConfirmShouldChangeChannel]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[TVControllerTest.PressMultipleDigitsAndConfirmShouldChangeChannel]=]  PROPERTIES DEF_SOURCE_LINE [==[C:\DEV_BR\TDD_TV_06\test\TVControllerTest.cpp:24]==] WORKING_DIRECTORY C:/DEV_BR/TDD_TV_06/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  TVControllerTest_TESTS TVControllerTest.Press1AndConfirmShouldChangeToChannel1 TVControllerTest.PressMultipleDigitsAndConfirmShouldChangeChannel)
--- test/TVControllerTest.cpp
+++ test/TVControllerTest.cpp
@@ -3,7 +3,6 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-
 class MockTunerForController : public Tuner {
 public:
   MOCK_METHOD(std::string, seekCH, (), (override));
@@ -14,12 +13,26 @@ public:
 TEST(TVControllerTest, Press1AndConfirmShouldChangeToChannel1) {
   MockTunerForController mockTuner;
 
-  // 1. 기대 정의: setCH("1")이 반드시 호출되어야 한다!
   EXPECT_CALL(mockTuner, setCH("1")).Times(1);
 
   TVController controller(&mockTuner);
 
-  // 2. 실행: 1번 누르고 OK 누르기
+  controller.pushButton(remoteKey::KEY_1);
+  controller.pushButton(remoteKey::KEY_OK);
+}
+
+TEST(TVControllerTest, PressMultipleDigitsAndConfirmShouldChangeChannel) {
+  MockTunerForController mockTuner;
+
+  // 1. 기대 정의: '1' 누르고 '2' 누르고 'OK' 누르면 -> setCH("12")가 호출되어야
+  // 함!
+  EXPECT_CALL(mockTuner, setCH("12")).Times(1);
+
+  TVController controller(&mockTuner);
+
+  // 2. 실행
+  controller.pushButton(remoteKey::KEY_1);
+  // 아직 KEY_2가 없으니 일단 KEY_1을 한 번 더 눌러 "11"을 만드는 테스트
   controller.pushButton(remoteKey::KEY_1);
   controller.pushButton(remoteKey::KEY_OK);
 }
