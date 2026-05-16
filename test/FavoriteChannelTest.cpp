80411a91c301b46c4090a2e13739c386d44338fc
yuldakim <jeong8003@gmail.com>
2026-05-16 21:40:18 +0900

    s3-2. Green / KEY_NEXT_FAV 기능 테스트 추가

--- test/FavoriteChannelTest.cpp
+++ test/FavoriteChannelTest.cpp
@@ -108,20 +108,23 @@ TEST_F(FavoriteChannelTest, S3_1_NextFavoriteChannel_Normal) {
 }
 
 TEST_F(FavoriteChannelTest, S3_2_NextFavoriteChannel_WhenCurrentIsInList) {
-  // 1. Given: 목록에 {6, 12, 37} 미리 세팅
+  // 1. Given: 명세서 데이터 세팅 {1, 4, 12, 56}
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("1"));
+  controller->pushButton(remoteKey::KEY_FAV);
+
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("4"));
+  controller->pushButton(remoteKey::KEY_FAV);
+
   EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("12"));
   controller->pushButton(remoteKey::KEY_FAV);
 
-  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("6"));
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("56"));
   controller->pushButton(remoteKey::KEY_FAV);
 
-  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("37"));
-  controller->pushButton(remoteKey::KEY_FAV);
-
-  // 2. 현재 채널을 목록에 존재하는 "12"로 설정
+  // 2. 현재 시청 채널을 목록에 있는 '12번'으로 설정
   EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("6"));
 
-  // 3. Then: 다음 버튼을 누르면 12 다음 값인 37번이 불려야 함!
+  // 3. Then: 12번에서 다음 버튼을 누르면 그 다음 큰 값인 '56번'으로 가야 함!
   EXPECT_CALL(tuner, setCH("12")).Times(1);
 
   // 4. When: 다음 선호 채널 버튼(≫) 클릭
