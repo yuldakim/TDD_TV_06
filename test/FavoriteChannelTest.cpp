adf7705e265173106ff5e2bd6571c8fa9a0fe8e4
yuldakim <jeong8003@gmail.com>
2026-05-16 21:29:01 +0900

    s3-1.Red / KEY_NEXT_FAV버튼이 케이스에 없음

--- test/FavoriteChannelTest.cpp
+++ test/FavoriteChannelTest.cpp
@@ -84,3 +84,25 @@ TEST_F(FavoriteChannelTest, S2_3_ComplexSequenceTest) {
   EXPECT_EQ(12, favs[1]);
   EXPECT_EQ(37, favs[2]);
 }
+
+TEST_F(FavoriteChannelTest, S3_1_NextFavoriteChannel_Normal) {
+  // 1. Given: 목록에 {6, 12, 37}을 미리 세팅하기 위해 버튼을 누릅니다.
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("12"));
+  controller->pushButton(remoteKey::KEY_FAV);
+
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("6"));
+  controller->pushButton(remoteKey::KEY_FAV);
+
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("37"));
+  controller->pushButton(remoteKey::KEY_FAV);
+  // 이 시점에서 내부 목록은 정렬되어 {6, 12, 37}이 됩니다.
+
+  // 2. 현재 채널을 "10"으로 설정 (12와 6 사이의 값)
+  EXPECT_CALL(tuner, getCurrentCH()).WillRepeatedly(Return("10"));
+
+  // 3. Then: 다음 선호 버튼을 누르면 10보다 큰 12번이 불려야 함!
+  EXPECT_CALL(tuner, setCH("12")).Times(1);
+
+  // 4. When: 다음 선호 채널 버튼(≫) 클릭
+  controller->pushButton(remoteKey::KEY_NEXT_FAV);
+}
